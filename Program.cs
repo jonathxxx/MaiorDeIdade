ac4785d4423e6b36181d8a273bfa84f575e26d7a
jonathxxx <jonatantenoriooficaltenorio@gmail.com>
2026-05-08 20:33:29 -0300

    BOATEKISS

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Me Fale a sua idade");
+int idade = Convert.ToInt32(Console.ReadLine());
+if (idade >= 18)
+{
+   Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Voce e maior de idade, pode entrar na boate kiss.");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Voce e menor, sai daqui moleque");
+}
+
+Console.ResetColor();
+Console.WriteLine("Obrigado!");
